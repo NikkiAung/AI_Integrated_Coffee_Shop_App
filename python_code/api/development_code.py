3b068d1530b99be742ad2183fdaf2102381cbc20
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2025-01-20 21:33:18 -0800

    develop: Classification Agent Successfully

--- python_code/api/development_code.py
+++ python_code/api/development_code.py
@@ -1,13 +1,15 @@
-from agents import GuardAgent
+from agents import (GuardAgent,
+                    ClassificationAgent)
 import os
 
 def main():
     guard_agent = GuardAgent()
-    
+    classification_agent = ClassificationAgent()
+
     messages = []
     while True:
         # Display the chat history
-        os.system('cls' if os.name == 'nt' else 'clear')
+        # os.system('cls' if os.name == 'nt' else 'clear')
         
         print("\n\nPrint Messages ...............")
         for message in messages:
@@ -23,6 +25,13 @@ def main():
         if guard_agent_response["memory"]["guard_decision"] == "not allowed":
             messages.append(guard_agent_response)
             continue
+        print("Pass Guard Test!")
+        # Get ClassificationAgent's response
+        classification_agent_response = classification_agent.get_response(messages)
+        chosen_agent=classification_agent_response["memory"]["classification_decision"]
+        print("Chosen Agent: ", chosen_agent)
+
+        
 
 # Add this to execute main
 if __name__ == "__main__":
